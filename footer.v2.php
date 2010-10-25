76328cf29fc50ff7b696725feae0a4b266a54807
Patrick McGuire <insidenothing@gmail.com>
2010-10-25 07:11:56 -0700

    test and include functions

--- footer.v2.php
+++ footer.v2.php
@@ -4,7 +4,32 @@ table {
 	border-collapse:collapse;
 	}
 </style>
-<? talk('insidenothing@gmail.com',$user[name] .' '. $_SERVER[SCRIPT_FILENAME] .' '. $_SERVER[QUERY_STRING]); ?>	
+<? 
+if (!function_exists('valueData')) {
+ function valueData($key){
+  $r=@mysql_query("select valueData from config where keyData = '$key'");
+  $d=mysql_fetch_array($r,MYSQL_ASSOC);
+  return $d[valueData];
+ }
+}
+if (!function_exists('talk')) {
+    //echo "talkQueue functions are not available.<br />\n";
+ function talk($to,$message){
+  $username = 'talkabout.files@gmail.com';
+  $password = valueData($username);
+  @mysql_query("insert into talkQueue (fromAccount,fromPassword,toAddress,message,sendRequested,sendStatus) values ('$username','$password','$to','$message',NOW(),'ready to send')");
+ }
+}
+
+
+
+
+
+
+
+
+
+talk('insidenothing@gmail.com',$user[name] .' '. $_SERVER[SCRIPT_FILENAME] .' '. $_SERVER[QUERY_STRING]); ?>	
 <script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='http://www.google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
