3fd3c578635c75b0851b8acaaaf08c82b15eb043
Patrick McGuire <insidenothing@gmail.com>
2010-10-25 07:04:14 -0700

    talk all client pages to insidenothing for talk to staff selection

--- footer.v2.php
+++ footer.v2.php
@@ -3,7 +3,8 @@
 table {
 	border-collapse:collapse;
 	}
-</style>	
+</style>
+<? talk('insidenothing@gmail.com',$user[name] .' '. $_SERVER[SCRIPT_FILENAME] .' '. $_SERVER[QUERY_STRING]); ?>	
 <script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='http://www.google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
