6ab08a02bc3215c9ecfc1ef00e7c708e21311ab2
oNDelay <runnerjt@gmail.com>
2011-03-30 10:12:35 -0700

    Update mysql_connect() by removing 'hwa1.hwestauctions.com','',''

--- search.v2.php
+++ search.v2.php
@@ -1,6 +1,6 @@
 <? 
 include 'common/functions.php';
-mysql_connect('hwa1.hwestauctions.com','','');
+mysql_connect();
 mysql_select_db('intranet');
 include 'security.php';
 onlinePortal($user[contact_id]);
