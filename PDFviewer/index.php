dc4fb02d9fa2b956673663213018007d0f05bf5f
J. Runner <runnerjt@gmail.com>
2011-07-22 13:18:08 -0700

    php53

--- PDFviewer/index.php
+++ PDFviewer/index.php
@@ -1,4 +1,4 @@
-<?
+<?PHP
 if ($_GET[pdf]){
 system('rm -f *.jpg');
 system('rm -f *.pdf');
@@ -7,86 +7,86 @@ system('cp '.$from.' page.pdf');
 $error = system('gs -dNOPAUSE -q -r75 -sDEVICE=jpeg -dBATCH -sOutputFile=page%d.jpg page.pdf', $retval);
 }
 ?>
-<? if (file_exists('page.jpg')){ ?>
+<?PHP if (file_exists('page.jpg')){ ?>
 <img src="page.jpg" style="position: relative" />
-<? 	$copy .= "<li>Rendered page.jpg</li>"; } ?>
+<?PHP 	$copy .= "<li>Rendered page.jpg</li>"; } ?>
 
-<? if (file_exists('page1.jpg')){ ?>
+<?PHP if (file_exists('page1.jpg')){ ?>
 <img src="page1.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page1.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page1.jpg</li>"; } ?>
 
-<? if (file_exists('page2.jpg')){ ?>
+<?PHP if (file_exists('page2.jpg')){ ?>
 <img src="page2.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page2.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page2.jpg</li>"; } ?>
 
-<? if (file_exists('page3.jpg')){ ?>
+<?PHP if (file_exists('page3.jpg')){ ?>
 <img src="page3.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page3.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page3.jpg</li>"; } ?>
 
-<? if (file_exists('page4.jpg')){ ?>
+<?PHP if (file_exists('page4.jpg')){ ?>
 <img src="page4.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page4.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page4.jpg</li>"; } ?>
 
-<? if (file_exists('page5.jpg')){ ?>
+<?PHP if (file_exists('page5.jpg')){ ?>
 <img src="page5.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page5.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page5.jpg</li>"; } ?>
 
-<? if (file_exists('page6.jpg')){ ?>
+<?PHP if (file_exists('page6.jpg')){ ?>
 <img src="page6.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page6.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page6.jpg</li>"; } ?>
 
-<? if (file_exists('page7.jpg')){ ?>
+<?PHP if (file_exists('page7.jpg')){ ?>
 <img src="page7.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page7.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page7.jpg</li>"; } ?>
 
-<? if (file_exists('page8.jpg')){ ?>
+<?PHP if (file_exists('page8.jpg')){ ?>
 <img src="page8.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page8.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page8.jpg</li>"; } ?>
 
-<? if (file_exists('page9.jpg')){ ?>
+<?PHP if (file_exists('page9.jpg')){ ?>
 <img src="page9.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page9.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page9.jpg</li>"; } ?>
 
-<? if (file_exists('page10.jpg')){ ?>
+<?PHP if (file_exists('page10.jpg')){ ?>
 <img src="page10.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page10.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page10.jpg</li>"; } ?>
 
-<? if (file_exists('page11.jpg')){ ?>
+<?PHP if (file_exists('page11.jpg')){ ?>
 <img src="page11.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page11.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page11.jpg</li>"; } ?>
 
-<? if (file_exists('page12.jpg')){ ?>
+<?PHP if (file_exists('page12.jpg')){ ?>
 <img src="page12.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page12.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page12.jpg</li>"; } ?>
 
-<? if (file_exists('page13.jpg')){ ?>
+<?PHP if (file_exists('page13.jpg')){ ?>
 <img src="page13.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page13.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page13.jpg</li>"; } ?>
 
-<? if (file_exists('page14.jpg')){ ?>
+<?PHP if (file_exists('page14.jpg')){ ?>
 <img src="page14.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page14.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page14.jpg</li>"; } ?>
 
-<? if (file_exists('page15.jpg')){ ?>
+<?PHP if (file_exists('page15.jpg')){ ?>
 <img src="page15.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page15.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page15.jpg</li>"; } ?>
 
-<? if (file_exists('page16.jpg')){ ?>
+<?PHP if (file_exists('page16.jpg')){ ?>
 <img src="page16.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page16.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page16.jpg</li>"; } ?>
 
-<? if (file_exists('page17.jpg')){ ?>
+<?PHP if (file_exists('page17.jpg')){ ?>
 <img src="page17.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page17.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page17.jpg</li>"; } ?>
 
-<? if (file_exists('page18.jpg')){ ?>
+<?PHP if (file_exists('page18.jpg')){ ?>
 <img src="page18.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page18.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page18.jpg</li>"; } ?>
 
-<? if (file_exists('page19.jpg')){ ?>
+<?PHP if (file_exists('page19.jpg')){ ?>
 <img src="page19.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page19.jpg</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page19.jpg</li>"; } ?>
 
-<? if (file_exists('page20.jpg')){ ?>
+<?PHP if (file_exists('page20.jpg')){ ?>
 <img src="page20.jpg" style="position: relative" />
-<? $copy .= "<li>Rendered page20.jpg, Should there be more pages??????</li>"; } ?>
+<?PHP $copy .= "<li>Rendered page20.jpg, Should there be more pages??????</li>"; } ?>
