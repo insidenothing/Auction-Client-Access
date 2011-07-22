b6ed5d7a17ccbbdd08e795a18a88729b7590cf0c
J. Runner <runnerjt@gmail.com>
2011-07-22 13:39:37 -0700

    php53

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div align="center" style="border-top:dashed">&copy; 1950 - 2009 Harvey West Auctioneers llc.<div class="noprint">Users Online Right Now:
-<?
+<?PHP
 $back = time()-1800;
 // ok let's show them their contacts
 mysql_select_db ('intranet');
@@ -7,8 +7,8 @@ $q="SELECT * FROM contacts WHERE online_now > '$back' AND attorneys_id = '$user[
 $r=@mysql_query($q);
 while ($d=mysql_fetch_array($r, MYSQL_ASSOC)){
 ?>
-<?=strtoupper(id2contact($d[contact_id]))?>,  
-<? }
+<?PHP echo strtoupper(id2contact($d[contact_id]))?>,  
+<?PHP }
 // now we need to show bound staff
 //mysql_select_db ('intranet');
 mysql_select_db ('intranet');
@@ -17,14 +17,14 @@ $q="SELECT * FROM users WHERE online_now > '$back'  ORDER BY name";
 $r=@mysql_query($q) or die(mysql_error);
 while ($d=mysql_fetch_array($r, MYSQL_ASSOC)){
 ?>
-<?=strtoupper(id2contact($d[contact_id]))?>,  
-<? }/* disabled for performance
+<?PHP echo strtoupper(id2contact($d[contact_id]))?>,  
+<?PHP }/* disabled for performance
 ?>
 <br />
 <script language="JavaScript" src="http://j.maxmind.com/app/geoip.js"></script>
 Local: <script language="JavaScript">document.write(geoip_city());</script>, <script language="JavaScript">document.write(geoip_region());</script>, <script language="JavaScript">document.write(geoip_country_name());</script>
 <br />
-<iframe src="pulse.php?uid=<?=$_GET[uid]?>" height="15px" frameborder="0"></iframe>
+<iframe src="pulse.php?uid=<?PHP echo $_GET[uid]?>" height="15px" frameborder="0"></iframe>
 */?></div>
 </div>
 <style>
