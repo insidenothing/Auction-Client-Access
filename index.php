2f04d619ca9f78d0523309c2c04c0d5710c51539
Patrick McGuire <insidenothing@gmail.com>
2011-07-25 08:03:07 -0700

    simple

--- index.php
+++ index.php
@@ -1,26 +1,4 @@
 <?PHP
-include 'header.php';
+header('Location: desktop.php');
 ?>
 
-<div style="font-size:20px; padding:5px;">
-  Welcome <?PHP echo $user[name]?>,<br /><br />
-</div>
-<br />
-
-
-<?PHP
-include 'footer.php';
-?>
-<script>
-
-function detectPopupBlocker() {
-  var myTest = window.open("about:blank","","directories=no,height=1,width=1,menubar=no,resizable=no,scrollbars=no,status=no,titlebar=no,top=0,location=no");
-  if (!myTest) {
-    alert("A popup blocker was detected. Please Allow http://portal.hwestauctions.com");
-  } else {
-    myTest.close();
-    //alert("No popup blocker was detected.");
-  }
-}
-window.onload = detectPopupBlocker;
-</script>
