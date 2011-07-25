b8ae378826e889a78d10a726d43c0c4f910a8acc
Patrick McGuire <insidenothing@gmail.com>
2011-07-25 08:00:53 -0700

    user_admin

--- security.php
+++ security.php
@@ -4,6 +4,7 @@ if (isset($_COOKIE['psportal']['name'])){
 $user = array(
 'name' => $_COOKIE['psportal']['name'],
 'contact_id' => $_COOKIE['psportal']['contact_id'],
+'user_admin' => $_COOKIE['psportal']['user_admin'],
 'attorneys_id' => $_COOKIE['psportal']['attorneys_id'],
 'email' => $_COOKIE['psportal']['email']
 );
