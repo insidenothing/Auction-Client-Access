eeef67f17a7f36ce8882a1ef66935b6f554e841e
Patrick McGuire <insidenothing@gmail.com>
2011-07-25 07:58:54 -0700

    cleaner

--- footer.v2.php
+++ footer.v2.php
@@ -21,21 +21,3 @@ if (!function_exists('talk')) {
  }
 }
 
-
-
-
-
-
-
-
-
-talk('insidenothing@gmail.com',$user[name] .' '. $_SERVER[SCRIPT_FILENAME] .' '. $_SERVER[QUERY_STRING]); ?>	
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='http://www.google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-1163096-8");
-pageTracker._trackPageview();
-} catch(err) {}</script>
