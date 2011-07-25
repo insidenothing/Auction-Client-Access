06b7bd2c3402eb630bebe113712df051449637ea
Patrick McGuire <insidenothing@gmail.com>
2011-07-25 06:58:31 -0700

    build user array

--- security.php
+++ security.php
@@ -1,4 +1,17 @@
 <?PHP
+
+if (isset($_COOKIE['psportal']['name'])){
+$user = array(
+'name' => $_COOKIE['psportal']['name'],
+'attorneys_id' => $_COOKIE['psportal']['attorneys_id'],
+'email' => $_COOKIE['psportal']['email']
+);
+
+}
+
+
+
+
 if (!$user[name]){
  hardLog(' Loaded '.$_SERVER[PHP_SELF].'+'.$_SERVER[QUERY_STRING ],'client');
  header('Location: http://portal.hwestauctions.com/login.php');
