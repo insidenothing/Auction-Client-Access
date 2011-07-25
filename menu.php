77cbbde98f13c35975a49acf255aa4bcaf3c1a10
Patrick McGuire <insidenothing@gmail.com>
2011-07-25 07:41:27 -0700

    strict

--- menu.php
+++ menu.php
@@ -1,11 +1,10 @@
-
 <table align="center" style=" border-collapse:collapse;" border="0" class="noprint">
 <form action="search.php">	
 	<tr>
-    	<td align="center"><? if ($_SERVER['PHP_SELF'] != "/portal/desktop.php"){ ?><a href="desktop.php?uid=<?=$uid?>"><?=$user[name]?>'s Desktop</a><? } ?></td>
+    	<td align="center"><?PHP if ($_SERVER['PHP_SELF'] != "/portal/desktop.php"){ ?><a href="desktop.php?uid=<?=$uid?>"><?=$user[name]?>'s Desktop</a><? } ?></td>
             <td align="center">
                 <input type="hidden" name="page" value="search" />
-                <input type="hidden" name="uid" value="<?=$_GET[uid]?>" />
+             
 				<script type='text/javascript'>document.write('<input name="resolution" value="'+screen.width+'x'+screen.height+'" type="hidden">');</script>
 
 				Search in
@@ -16,7 +15,7 @@
 				<option value="sd">Sale Date YYYY-MM-DD</option>
 				<option value="an">Auction Number</option>
 				</select>
-				 for <input size="20" style="font-weight:bold; font-variant:small-caps" name="q" value="<?=$_GET[q];?>" />
+				 for <input size="20" style="font-weight:bold; font-variant:small-caps" name="q" value="<?php echo $_GET[q];?>" />
 				<input style=" font-weight:bold; font-variant:small-caps" type="submit"  value="Search Auction Database"/><br /><strong>Did you know? When searching for files you can use the percent sign '%' as a wildcard!</strong>
             </td>
             <td align="left"><a href="logout.php?uid=<?=$_GET[uid]?>"><img border="0" src="gfx/exit.JPG" height="50px" width="100px"></a></td>
