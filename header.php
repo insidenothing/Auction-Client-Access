43aca3ff6f3add066d2e566e3038260d0825651d
Patrick McGuire <insidenothing@gmail.com>
2011-07-25 06:01:38 -0700

    back alive

--- header.php
+++ header.php
@@ -1,13 +1,10 @@
 <?PHP
-header('Location: http://hwestauctions.com');
 include 'common/functions.php';
-//db_connect('hwa1.hwestauctions.com','intranet','','');
 mysql_connect();
 mysql_select_db('intranet');
 include 'security.php';
 onlinePortal($user[contact_id]);
 hardLog(id2attorneys($user[attorneys_id]).'] ['.$user[name].' Loaded '.$_SERVER[PHP_SELF].'+'.$_SERVER[QUERY_STRING ],'client');
-
 ?>
 <style>
 body {margin:0px; padding:0px;}
@@ -21,8 +18,7 @@ a:hover {text-decoration:none; color:#000000; font-weight:bold;}
       .noprint { display: none; }
     }
   </style> 
-  <link href='http://mdwestserve.com/favicon.gif' TYPE='image/gif' REL='icon'>
-<center><div style="border:ridge 3px #000000; background-color:#cc9900; font-size:20px; width:400px;" align="center"><a href="http://mdwestserve.com" style="text-decoration:none; color:#000000;">Switch to Process Service</a></div></center>
+<link href='favicon.gif' TYPE='image/gif' REL='icon'>
 <table align="center" cellpadding="0px" cellspacing="0px"><tr><td valign="top" style="padding-left:5px;padding-right:5px; border:ridge 5px #006699;">
 
 <?PHP
