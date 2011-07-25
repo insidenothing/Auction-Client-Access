9f0d206ee1b934dfaeb5f85314b64a681d96e368
Patrick McGuire <insidenothing@gmail.com>
2011-07-25 08:08:24 -0700

    cleaner

--- invoice.v2.php
+++ invoice.v2.php
@@ -1,11 +1,11 @@
-<?
+<?PHP
 include 'header.v2.php';
 portal_log("Accessing Invoice for ".$_GET[filename], $user[contact_id]);
 ?>
 <center>
-<iframe src="http://staff.hwestauctions.com/write_invoice.php?id=<?=$_GET[auction]?>" width="400" height="200" frameborder="0"></iframe>
+<iframe src="http://staff.hwestauctions.com/write_invoice.php?id=<?PHP echo $_GET[auction]?>" width="400" height="200" frameborder="0"></iframe>
 <br>
-<a href="simpleDetails.v2.php?id=<?=$_GET[auction]?>">[Take Me Back to Auction Details]</a>
+<a href="simpleDetails.v2.php?id=<?PHP echo $_GET[auction]?>">[Take Me Back to Auction Details]</a>
 </center>
 
-<? include 'footer.v2.php';?>
+<?PHP include 'footer.v2.php';?>
