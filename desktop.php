0986a1fd149555f6ed436b09335b78f33742e316
Patrick McGuire <insidenothing@gmail.com>
2011-07-25 07:03:25 -0700

    php53

--- desktop.php
+++ desktop.php
@@ -1,4 +1,4 @@
-<?
+<?PHP
 header('Location: http://hwestauctions.com');
 
 include 'common/functions.php';
@@ -26,7 +26,7 @@ a:hover {text-decoration:none; color:#000000; font-weight:bold;}
  <center> <div style="border:ridge 3px #000000; background-color:#cc9900; font-size:20px; width:400px;" align="center"><a href="http://mdwestserve.com" style="text-decoration:none; color:#000000; width:400px;" align="center">Switch to Process Service</a></div></center>
 <table width="80%" align="center" cellpadding="0px" cellspacing="0px"><tr><td valign="top" style="padding-left:5px;padding-right:5px; border:ridge 5px #006699;">
 
-<?
+<?PHP
 include 'menu.php';
 ?>
 
@@ -35,28 +35,28 @@ fieldset, legend {border:none; padding:5px;; font-size:20px;}
 </style>
 <table align="center" width="100%"><tr><td valign="top" width="99%">
 <fieldset>
-	<legend><?=$user[name]?>'s Auction Desktop</legend>
-		<li><a href="dailyInvoices.php?uid=<?=$_GET[uid]?>">Daily Invoice Lists</a> <small>[added 4/16/2009]</small></li>
+	<legend><?PHP echo $user[name]?>'s Auction Desktop</legend>
+		<li><a href="dailyInvoices.php?uid=<?PHP echo $_GET[uid]?>">Daily Invoice Lists</a> <small>[added 4/16/2009]</small></li>
 
-		<? if ($user[attorneys_id] == "1"){?>
-			<li><a href="csvtesting.php?uid=<?=$_GET[uid]?>">Test Exported CSV against Harvey West Database</a></li>
-			<li><a href="upload-burson.php?uid=<?=$_GET[uid]?>">Place Order for Auction</a></li>
-		<? }else{?> 
-			<li><a href="upload.php?uid=<?=$_GET[uid]?>">Place Order for Auction</a></li>
-		<? } ?>
-		<li><a href="transfer_history.php?uid=<?=$_GET[uid]?>">Transfers Log</a></li>
-		<? if ($user[user_admin] == "YES"){?>
-			<li><a href="users.php?uid=<?=$_GET[uid]?>">User Management</a></li>
-			<li><a href="settings.php?uid=<?=$_GET[uid]?>">Client Options</a></li>
-		<? } ?>
-		<li><a href="deadlines.php?uid=<?=$_GET[uid]?>">New Auction Deadlines</a></li> 
-		<? if ($user[attorneys_id] == '21'){?>
-			<li><a href="report-draper-md.php?uid=<?=$_GET[uid]?>">MD Print Cost</a></li>
-			<li><a href="report-draper-dc.php?uid=<?=$_GET[uid]?>">DC Print Cost</a></li>       
-		<? }?>
+		<?PHP if ($user[attorneys_id] == "1"){?>
+			<li><a href="csvtesting.php?uid=<?PHP echo $_GET[uid]?>">Test Exported CSV against Harvey West Database</a></li>
+			<li><a href="upload-burson.php?uid=<?PHP echo $_GET[uid]?>">Place Order for Auction</a></li>
+		<?PHP }else{?> 
+			<li><a href="upload.php?uid=<?PHP echo $_GET[uid]?>">Place Order for Auction</a></li>
+		<?PHP } ?>
+		<li><a href="transfer_history.php?uid=<?PHP echo $_GET[uid]?>">Transfers Log</a></li>
+		<?PHP if ($user[user_admin] == "YES"){?>
+			<li><a href="users.php?uid=<?PHP echo $_GET[uid]?>">User Management</a></li>
+			<li><a href="settings.php?uid=<?PHP echo $_GET[uid]?>">Client Options</a></li>
+		<?PHP } ?>
+		<li><a href="deadlines.php?uid=<?PHP echo $_GET[uid]?>">New Auction Deadlines</a></li> 
+		<?PHP if ($user[attorneys_id] == '21'){?>
+			<li><a href="report-draper-md.php?uid=<?PHP echo $_GET[uid]?>">MD Print Cost</a></li>
+			<li><a href="report-draper-dc.php?uid=<?PHP echo $_GET[uid]?>">DC Print Cost</a></li>       
+		<?PHP }?>
 		<li><a href="mailto:patrick@hwestauctions.com">Send Feedback</a></li>
-		<li><a href="log.php?uid=<?=$_GET[uid]?>">Activity Log</a></li>
+		<li><a href="log.php?uid=<?PHP echo $_GET[uid]?>">Activity Log</a></li>
 </fieldset>
-<?
+<?PHP
 include 'footer.php';
 ?>
