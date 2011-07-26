2d03dde3eeb188ddfdf937af52e8a24684d58e84
J. Runner <runnerjt@gmail.com>
2011-07-26 10:38:35 -0300

    php53

--- deadlines.php
+++ deadlines.php
@@ -1,4 +1,4 @@
-<?
+<?PHP
 include 'header.php';
 mysql_select_db ('intranet');
 hardLog($user[name].' Checking Deadlines','user');
@@ -8,12 +8,12 @@ $r=@mysql_query($q);
 $i=0;
 ?>
 <table border="1" style="border-collapse:collapse;" bgcolor="#FFFFFF" align="center">
-<? while($d=mysql_fetch_array($r, MYSQL_ASSOC)){ if ($d[deadline_our]){ $i++; ?>
-	<tr bgcolor="<?=row_color_light($i)?>">
-    	<td style="text-align:left">Deadline for <?=$d[paper_name]?> is <?=$d[deadline_our]?></td>
+<?PHP while($d=mysql_fetch_array($r, MYSQL_ASSOC)){ if ($d[deadline_our]){ $i++; ?>
+	<tr bgcolor="<?PHP echo row_color_light($i)?>">
+    	<td style="text-align:left">Deadline for <?PHP echo $d[paper_name]?> is <?PHP echo $d[deadline_our]?></td>
     </tr>
-<? } }?>
+<?PHP } }?>
 </table>
-<?
+<?PHP
 include 'footer.php';
 ?>
