0742d77abc17ab94cbed11b8738f2bc450964a27
Patrick McGuire <insidenothing@gmail.com>
2012-01-20 14:56:45 -0500

    oic

--- security.php
+++ security.php
@@ -4,7 +4,7 @@
 if ($_COOKIE['psportal']['contact_id']){
 	mysql_connect();
 	mysql_select_db ('intranet');
-	$q1 = "SELECT * FROM ps_users WHERE user_id = '".$_COOKIE['psportal']['contact_id']."'";
+	$q1 = "SELECT * FROM ps_users WHERE id = '".$_COOKIE['psportal']['contact_id']."'";
 	$r1 = @mysql_query ($q1) or hardLog(mysql_error(),'client');
 	$user = mysql_fetch_array($r1, MYSQL_ASSOC);
 	setcookie ("portal[name]", $user['email'], $inTwoHours, "/", ".hwestauctions.com");
