59e0a6ac4b347d7e2b5aa9d0a9c489876ed77fde
Patrick McGuire <insidenothing@gmail.com>
2012-01-20 14:56:31 -0500

    ok

--- security.php
+++ security.php
@@ -2,12 +2,12 @@
 
 // build $user array from cookie
 if ($_COOKIE['psportal']['contact_id']){
-mysql_connect();
-mysql_select_db ('intranet');
-$q1 = "SELECT * FROM ps_users WHERE user_id = '".$_COOKIE['psportal']['contact_id']."'";
-$r1 = @mysql_query ($q1) or hardLog(mysql_error(),'client');
-$user = mysql_fetch_array($r1, MYSQL_ASSOC);
-setcookie ("portal[name]", $user['email'], $inTwoHours, "/", ".hwestauctions.com");
+	mysql_connect();
+	mysql_select_db ('intranet');
+	$q1 = "SELECT * FROM ps_users WHERE user_id = '".$_COOKIE['psportal']['contact_id']."'";
+	$r1 = @mysql_query ($q1) or hardLog(mysql_error(),'client');
+	$user = mysql_fetch_array($r1, MYSQL_ASSOC);
+	setcookie ("portal[name]", $user['email'], $inTwoHours, "/", ".hwestauctions.com");
 }
 
 
@@ -15,8 +15,7 @@ setcookie ("portal[name]", $user['email'], $inTwoHours, "/", ".hwestauctions.com
 
 
 if (!$user[name]){
-hardLog(' Loaded '.$_SERVER[PHP_SELF].'+'.$_SERVER[QUERY_STRING ],'client');
-header('Location: http://hwestauctions.com');
+	header('Location: http://hwestauctions.com');
 }
 
 
