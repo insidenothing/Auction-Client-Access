d29720fd68ebae236a26633a5ec2b9984e48af54
Patrick McGuire <insidenothing@gmail.com>
2012-01-20 14:34:31 -0500

    ok

--- security.php
+++ security.php
@@ -3,8 +3,8 @@ if(!$_COOKIE[psportal][debug]){
 
 if (!$_GET[uid]){
 if (!$_POST[uid]){
-portal_log("Security Active :: UID Not Found", 0);
-header ('Location: http://hwestauctions.com?logout=noUID');
+//portal_log("Security Active :: UID Not Found", 0);
+header ('Location: http://hwestauctions.com');
 }
 }
 if ($_GET[uid]){
