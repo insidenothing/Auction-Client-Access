243f11209d87b356b06ea1ca9e383bc199ae86dd
Patrick McGuire <insidenothing@gmail.com>
2012-07-27 12:06:30 -0400

    smaller

--- search.v2.php
+++ search.v2.php
@@ -82,7 +82,9 @@ echo "
 
 <td nowrap>$data1[sale_date] $data1[sale_time]</td>";
 
-echo '<td nowrap>'.$data1['auctioneer'].' '.auctioneerPhone($data1['auctioneer']).' '.$data1['auctioneer2'].' '.auctioneerPhone($data1['auctioneer2'].' '.$data1['auctioneer3'].' '.auctioneerPhone($data1['auctioneer3']).'</td>';
+echo '<td nowrap>'.$data1['auctioneer'].' '.auctioneerPhone($data1['auctioneer']);
+echo $data1['auctioneer2'].' '.auctioneerPhone($data1['auctioneer2'];
+echo $data1['auctioneer3'].' '.auctioneerPhone($data1['auctioneer3']).'</td>';
 
 echo "<td style='text-align:left;' nowrap>".substr($data1[address1],0,30)."</td>
 <td style='text-align:left;' nowrap>$data1[county]</td>
